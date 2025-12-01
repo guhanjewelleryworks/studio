88a861d51169a422eb4d1b273681952f5616eb74
Guhan Jewellery Works <guhanjewelleryworks@gmail.com>
2025-12-01 17:47:11 +0000

    hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -97,7 +97,7 @@ export default function Home() {
         <section className="relative w-full h-[75vh] min-h-[600px] max-h-[800px] flex items-center justify-center text-white">
             <div className="absolute inset-0 z-0">
                 <Image
-                    src="/images/goldsmith_hero_right.png"
+                    src="/images/heroimage.png"
                     alt="A skilled goldsmith meticulously crafting a piece of jewelry."
                     fill
                     className="object-cover"
